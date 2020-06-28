c4778c781416f72a16de9867e947252f7d043030
jabacousnac <55156956+jabacousnac@users.noreply.github.com>
2020-06-27 20:52:29 -0400

    Add files via upload

--- find_intensity_scaled_units.py
+++ find_intensity_scaled_units.py
@@ -4,14 +4,14 @@
 import scipy.special as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.lines as lines 
 from numpy import fft
-from mpl_toolkits.mplot3d import Axes3D
 
 #work in SI Units
 #m = 1
 k = 6.905*(10**6) #corresponds to 1064 nm
 f = 0.5 
-R = 0.1
+R = 0.09
 
 
 def find_U0(m, lim):
@@ -54,16 +54,45 @@ def find_Uz(m,z,lim):
             H_array[i,j] = np.exp(z*1j*(k**2-(2*np.pi*s)**2)**0.5) #this expression is FT of h(x,y,z)
     prod = H_array*U_0
     #final step: Find the inverse fourier transform of this guy
-    U_z = fft.ifft2(prod) #change to prod
-    #Plotting:
-    plt.figure()
-    plt.imshow(((np.imag(U_z)**2))+((np.real(U_z)**2)), cmap = 'gray', interpolation = 'nearest', extent=[-lim,lim,-lim,lim])
-    plt.title("Intensity of field at z=" + str(z) + ", " + "m=" + str(m))
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.colorbar()
-    plt.show(block = False) #so we can display multiple figures to compare
-    return U_z
+    U_z = fft.ifft2(prod)
+    Intensity_array = np.real(U_z)**2 + np.imag(U_z)**2
+    if False: #Plotting: (change to True if we want to plot)
+        plt.figure()
+        plt.imshow(((np.imag(U_z)**2))+((np.real(U_z)**2)), cmap = 'gray', interpolation = 'nearest', extent=[-lim,lim,-lim,lim])
+        plt.title("Intensity of field at z=" + str(z) + ", " + "m=" + str(m))
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.colorbar()
+        plt.show(block = False) #so we can display multiple figures to compare
+    return Intensity_array
+
+#3
+def get_int (coord,m):
+    #this function takes in a pair of (x,y) values in meters and outputs an array of intensity values along with corresponding z; m is topological charge
+    x,y = coord[0],coord[1]
+    z = 0
+    zmax = 20*f
+    dz = 0.1
+    intlist,zlist = list(),list()
+    while z < zmax+dz:
+        lim = max([x,y])+1 #generate smallest array necessary
+        Uz = find_Uz(m,z,lim)
+        i = x*2*lim/(np.shape(Uz)[0]-1)
+        j = y*2*lim/(np.shape(Uz)[1]-1)
+        intensity = np.real(Uz[int(i),int(j)])**2 + np.imag(Uz[int(i),int(j)])**2
+        zlist.append(z)
+        intlist.append(intensity)
+        z+=dz
+    fig, ax = plt.subplots(figsize=(8,8))
+    ax.scatter(zlist, intlist)
+    ax.plot([f,f],[0,max(intlist)],'r--')
+    ax.set_title("Intensity of field, " + "m=" + str(m)+ ", for (x,y) = " + str([x,y]))
+    ax.set_xlabel("z")
+    ax.set_ylabel("Intensity")
+    plt.show()
+    return intlist
+        
+
 
 #if __name__ == "__main__":
  #   find_Uz(1,0.5,0.5)
