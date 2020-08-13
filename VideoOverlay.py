bd3c8e12223ffec66640b3e1a5ad4087d9ce6e3e
jabacousnac <55156956+jabacousnac@users.noreply.github.com>
2020-08-13 14:29:57 -0400

    Add files via upload
    Now has the capability of overlaying

--- VideoOverlay.py
+++ VideoOverlay.py
@@ -3,6 +3,7 @@ import os
 import datetime
 from matplotlib import pyplot as plt
 import json
+import re
 
 """Convert a video into frames, add trap positions, then convert back into a video"""
 #code adapted from https://medium.com/@iKhushPatel/convert-video-to-images-images-to-video-using-opencv-python-db27a128a481
@@ -14,9 +15,9 @@ def getFrames(sec):
     vidcap.set(cv2.CAP_PROP_POS_MSEC, sec * 1000)
     hasFrames, image = vidcap.read()
     if hasFrames:
-		print(count)
-		fn = str(time)[0:10] + '_t' + str(count) + '.png'
-		cv2.imwrite(fn, image)
+        print(count)
+        fn = str(time)[0:10] + '_t' + str(count) + '.png'
+        cv2.imwrite(fn, image)
     return hasFrames
 
 '''
@@ -32,28 +33,55 @@ while success:
 '''
 
 def get_pos():
-	"""Obtain the trap positions in the form of 2D vectors, X and Y"""
-	with open('1.json', 'w') as jsonfile: #change to json filename
-		data = json.load(jsonfile)
-		print(data)
+    """Obtain the trap positions in the form of 2D vectors, X and Y"""
+    with open('2.json','r') as jsonfile: #change to actual json filename
+        data = jsonfile.read()
+        data = re.sub(r'\s+','',data)
+        X, Y = list(), list()
+        bool = True
+        while bool:
+            if data != '':
+                pos = ((data.split('['))[1].split(']')[0]).split(',')
+                X.append(float(pos[0])); Y.append(float(pos[1]));
+                index = data.find(']')
+                data = data[index+1:]
+            else:
+                bool = False
+    return[X,Y]
+
+def extension_count(extension):
+    """returns the number of files found in directory with that extension"""
+    myPath = os.getcwd()
+    filelist = [f for f in os.listdir(myPath) if f.endswith(extension) and os.path.isfile(os.path.join(myPath,f))]
+    return len(filelist)
 
 def overlay():
     """Given the trap positions, now overlay on the frames"""
     directory = os.getcwd()
-    for root, dirs, files in os.walk(directory):
-        for filename in files:
-            if filename.endswith('png'): #ensure that this is the only file of that filetype
-                X = plt.imread(filename)
-                fig, ax = plt.subplots()
-                ax.imshow(X)
-                ax.axis('off')
-                plt.plot([0], [123], 'ro')
-                plt.show()
+    frame = 0
+    X, Y = get_pos()[0], get_pos()[1]
+    if extension_count('.png') == len(X):
+        bool = True
+    else:
+        bool = False
+    print(bool)
+    while bool:
+        for root, dirs, files in os.walk(directory):
+            for filename in files:
+                if filename.endswith('png'): #ensure that these images are the only ones with the extension
+                    x,y = X[frame], Y[frame]
+                    frame += 1
+                    im = plt.imread(filename)
+                    fig, ax = plt.subplots()
+                    ax.imshow(im)
+                    ax.axis('off')
+                    plt.plot(x, y, 'ro')
+                    plt.savefig(filename) #overwrite the existing image
     return
 
 def stitchFrames():
     """Convert the frames into a video"""
 
 if __name__ == '__main__':
-    get_pos()
     #getFrames(0)
+    overlay()
