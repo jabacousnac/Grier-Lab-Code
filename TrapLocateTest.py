70c91d8f5d78e3f2df13ec602563317bc248f596
jabacousnac <55156956+jabacousnac@users.noreply.github.com>
2020-08-17 15:21:25 -0400

    Add files via upload

--- TrapLocateTest.py
+++ TrapLocateTest.py
@@ -2,25 +2,25 @@ from .Task import Task
 from PyQt5.QtGui import QVector3D #data type for trap.r
 import numpy as np
 import json
-import os.path
-from os import path
 
 class TrapLocateTest(Task):
 	"""Obtain the coordinates of where the trap currently is and saves to a .json file"""
 
-	def __init__(self, traps=None, ntraps=None, **kwargs):
+	def __init__(self, fn=None, traps=None, ntraps=None, **kwargs):
 		super(TrapLocateTest, self).__init__(**kwargs)
+		self.fn = fn
 		self.traps = None
 		self.ntraps = ntraps
 	
 	def dotask(self):
 		self.traps = self.parent.pattern.pattern
 		self.ntraps = self.traps.count()
-		fname = 'mysillyexperiment' + '.json' #change this
+		if self.fn is None:
+			self.fn = 'pyfab'
 		if self.ntraps > 0:
 			for trap in self.traps.flatten():
 				sym = trap.plotSymbol() #no worries, we'll use this info later
-				coord = ((sym, trap.r.x(), trap.r.y(), trap.r.z())) 
+				coord = ((trap.r.x(), trap.r.y(), trap.r.z())) 
 			print(coord)
-			with open(fname, 'a') as myfile:
+			with open(self.fn, 'a') as myfile:
 					json.dump(coord, myfile)
