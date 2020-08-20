8656661e6f260d3b511c93b0612c275471ff02ce
jabacousnac <55156956+jabacousnac@users.noreply.github.com>
2020-08-20 16:35:39 -0400

    Add files via upload
    Updated, with Jackie's help. Only issue: video keeps recording

--- RecordTrapTest.py
+++ RecordTrapTest.py
@@ -4,39 +4,35 @@
 from .Task import Task
 from PyQt5.QtGui import QVector3D
 import numpy as np
+import json
 import datetime as dt
 
-time = dt.datetime.now()
-fname = str(time)[:-7]
 
 class RecordTrapTest(Task):
     """Translate trap, record and save its position in JSON file, while recording"""
 
-    def __init__(self, measure_bg=False, **kwargs):
+    def __init__(self, **kwargs):
         super(RecordTrapTest, self).__init__(**kwargs)
-        self.traps = None
-        self.measure_bg = False 
-        self.nframes = 1000
-
+        self.nframes = 500                     
+        self.dr = QVector3D(0.5, 0.5, 0.)
+    
     def initialize(self, frame):
-        self.traps = self.parent.pattern.pattern
-        self.ntraps = self.traps.count()
-        xc = self.parent.cgh.device.xc
-        trap = self.traps.flatten()[0]
-        self.r = np.array((trap.r.x(), trap.r.y()))
+        time = dt.datetime.now()               
+        self.dvr = self.parent.dvr             
+        self.dvr.filename = str(time)[:10] + '.avi'
+        self.dvr.recordButton.animateClick()
+        self.time = time
 
-    def dotask(self):
-        self.traps = self.parent.pattern.pattern
-        self.ntraps = self.traps.count()
-        dx, dy, dz, framenum = 0.5, 0.0, 0.0, 0
-        self.nframes = 500
-        dr = QVector3D(dx,dy,dz)
-        self.register('Record', fn = fname + '.avi', stop = False)
-        while framenum < self.nframes:
-            self.register('Translate', traps=self.traps, dr=dr)
-            self.register('TrapLocateTest', fn=fname, traps=self.traps, ntraps=self.ntraps)
-            framenum += 1
-
-
-                 
-
+    def doprocess(self, frame): #use self.dvr._framenumber to obtain the frame at which we're at
+        traps = self.parent.pattern.pattern
+        if traps.count() > 0:                   
+            trap = traps.flatten()[0]
+            coord = ((trap.r.x(), trap.r.y(), trap.r.z())) 
+            trap.moveBy(self.dr)
+            with open(str(self.time)[:10] + '--' + str(self.time)[11:-7] + '.json', 'a') as myfile:
+                json.dump(coord, myfile)
+        else:
+            print('Error: no traps to translate!') 
+            coord = None
+        
+            
