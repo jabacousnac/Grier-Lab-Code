f46e380ccb2c329eb42a2eb281ecd3117829ce25
jabacousnac <55156956+jabacousnac@users.noreply.github.com>
2020-11-30 14:39:55 -0500

    Add files via upload
    Can choose the trap of interest

--- PlotTrajectories.py
+++ PlotTrajectories.py
@@ -1,17 +1,18 @@
-import numpy as np
 from matplotlib import pyplot as plt
 import re
 import json
 from ParticleGetter import *
 import os
+import numpy as np
 
 def plotter(ID):
     #first obtain trap coordinates
-    fname = '2020-09-16--14-47-43'
+    fname = '2020-11-13--17-08-57'
+    trap_ID = 1 #there are n traps and we can choose one of these traps
     dx, dy, dz = 0.0, 0.0, 0.0 #for uncalibrated traps
     with open(fname + '.json', 'r') as TrapCoord:
         data = TrapCoord.read()
-        data = re.sub(r'\s+', '', data)
+        data = re.sub(r'\s+ ', '', data)
         data = data[1:-1]
         Xt, Yt, Zt = [], [], []
         counter = 0
@@ -19,13 +20,19 @@ def plotter(ID):
         while bool:
             if data != '':
                 counter+=1
-                #pos = ((data.split('['))[0].split(']')[0]).split(',')
-                pos = data.split('[[')[1].split(']]')[0].split(',')
-                Xt.append(float(pos[0]) - dx)
-                Yt.append(float(pos[1]) - dy)
-                Zt.append(float(pos[2]) - dz)
-                index = data.find(']]')
-                data = data[index+2:]
+                my_index = data.find(']]')
+                frame = data[1:my_index+1] #this is trap coordinates [x1,y1,z1], [x2,y2,z2] for frame
+                #now, we have to splice frame to get trap coordinates of interest
+                #we get a new data:
+                data = data[my_index+4:]
+                pos = frame[1:-1].split('], [')[trap_ID]
+                more_pos = pos.split(',')
+                Xt.append(float(more_pos[0])-dx)
+                Yt.append(float(more_pos[1])-dy)
+                Zt.append(float(more_pos[2])-dz)
+                ##pos = ((data.split('['))[0].split(']')[0]).split(',')
+                #pos = data.split('[[')[1].split(']]')[0].split(',')
+                #print(pos)
             else:
                 bool = False
         TrapCoord.close()
@@ -41,29 +48,41 @@ def plotter(ID):
             Yp.append(PreDict['y_p'])
             Zp.append(PreDict['z_p'])
             frame+=1
-            print(frame)
     #If we have multiple particles
     df = find_trajs()
     Xp = [col[1] for row,col in df.iterrows() if col[5] == ID]
     Yp = [col[2] for row,col in df.iterrows() if col[5] == ID]
     Zp = [col[3] for row,col in df.iterrows() if col[5] == ID]
+    #bin data:
+    bindex = 0
+    spacing = 100
+    Xp_plot, Xt_plot, Yp_plot, Yt_plot, Zp_plot, Zt_plot  = [], [], [], [], [], []
+    for bindex in range(len(Xt)-spacing):
+        Xt_plot.append(np.average(Xt[bindex:bindex+spacing+1]))
+        Yt_plot.append(np.average(Yt[bindex:bindex+spacing+1]))
+        Zt_plot.append(np.average(Zt[bindex:bindex+spacing+1]))
+        Xp_plot.append(np.average(Xp[bindex:bindex+spacing+1]))
+        Yp_plot.append(np.average(Yp[bindex:bindex+spacing+1]))
+        Zp_plot.append(np.average(Zp[bindex:bindex+spacing+1]))
+        bindex+=spacing+1
     #plot
-    plt.plot(Xt, 'r-')
-    plt.plot(Yt, 'b-')
-    plt.plot(Zt, 'g-')
-    plt.plot(Xp, 'r--')
-    plt.plot(Yp, 'b--')
-    plt.plot(Zp, 'g--')
-    title = 'Trap/Particle Trajectories_' + fname + ' ' + 'Particle ID = ' + str(ID)
+    plt.plot(Xt_plot, 'r-')
+    plt.plot(Yt_plot, 'b-')
+    plt.plot(Zt_plot, 'g-')
+    plt.plot(Xp_plot, 'r--')
+    plt.plot(Yp_plot, 'b--')
+    plt.plot(Zp_plot, 'g--')
+    title = 'Trajectories_' + fname + ', ' + 'P-ID = ' + str(ID) + ' ' + 'T-ID = ' + str(trap_ID)
     plt.title(title)
     plt.ylabel('Position (pixels)')
-    plt.xlabel('Time (frames)')
+    plt.xlabel('Time') #in number of frames/scaling
     plt.legend(('$x_{t}$', '$y_{t}$', '$z_{t}$', '$x_{p}$', '$y_{p}$', '$z_{p}$'))
     folder_path = path + '/trajectories/'
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-    plt.savefig(folder_path + 'trajs_particle_ID#: ' + str(ID) + '.png', dpi = 'figure')
+    plt.savefig(folder_path + 'p_ID#:' + str(ID) + '_t_ID#:' + str(trap_ID) + '.png', dpi = 'figure')
+    return [Xt, Xp, Yt, Yp, Zt, Zp]
 
 if __name__ == '__main__':
     path = os.getcwd()
-    plotter(2) #change the particle ID
+    plotter(0) #change the particle ID
