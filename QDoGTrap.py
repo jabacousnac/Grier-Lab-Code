2a488857c16a386288fad6238cb0fa0ec365e660
jabacousnac <55156956+jabacousnac@users.noreply.github.com>
2021-02-20 00:18:20 -0500

    Add files via upload
    Changed to appropriate units

--- QDoGTrap.py
+++ QDoGTrap.py
@@ -6,12 +6,13 @@ from .QTrap import QTrap
 import numpy as np
 from PyQt5.QtCore import pyqtProperty
 from PyQt5.QtGui import (QPainterPath, QFont, QTransform)
-from scipy.special import jv
+#import sys
+#np.set_printoptions(threshold=sys.maxsize)
 
 
 class QDoGTrap(QTrap):
 
-    def __init__(self, sigma=20, tau=10, alpha=50, **kwargs):
+    def __init__(self, sigma=40, tau=20, alpha=50, **kwargs):
         super(QDoGTrap, self).__init__(alpha=alpha, **kwargs)
         self._sigma = sigma
         self._tau = tau
@@ -19,12 +20,20 @@ class QDoGTrap(QTrap):
         self.registerProperty('tau', tooltip=True)
 
     def updateStructure(self):
-        kappa = 1j * np.pi / (1064*10**-9 * 10**-6) #i*pi/lambda*f
-        alpha, beta = 1/(self.sigma*10**-6)**2 - kappa, 1/(self.tau*10**-6)**2 - kappa
-        arg = (alpha * np.exp(-alpha * (self.cgh.qr)**2)) + \
-        (beta * np.exp(-beta * (self.cgh.qr)**2))
-        phi = np.exp(1j * np.arctan(arg))
-        self.structure = phi
+#1. real units -> pixels
+        wavelength = 1064e-9 / (48e-9)
+        z = 1e-7 / (48e-9)
+#2. kappa, a, b are quantities to simplify calculations
+        kappa = 1j * np.pi /(wavelength * z)
+        a, b = 1/(self.sigma)**2 - kappa, 1/(self.tau)**2 - kappa
+#3. get phase part
+        arg = (a * np.exp(-a * (self.cgh.qr)**2)) - \
+        (b * np.exp(-b * (self.cgh.qr)**2))
+#4. get amplitude part
+        u = -kappa * np.exp((1j * 2*np.pi/wavelength * z) + kappa * (self.cgh.qr)**2) * \
+        (1/a * np.exp(kappa**2 * (self.cgh.qr)**2 / a) - 1/b * np.exp(kappa**2 * (self.cgh.qr)**2 / b))
+        self.structure = np.exp(1j * np.angle(arg)) * u
+        print (np.exp(1j * np.angle(arg)) * u)
 
 
     def plotSymbol(self):
